717294783929a6665993a619669ec3582bad2add
AUSTINMORARA <austiinmorara@gmail.com>
2022-09-14 17:03:14 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *prints_alphabet - functions to print abc
+ *
+ *Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putcher('\n');
