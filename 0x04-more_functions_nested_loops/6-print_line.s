e18e433a51c13cff245c32046a88bb5b956c73af
AUSTINMORARA <austiinmorara@gmail.com>
2022-09-15 16:39:55 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.s
@@ -0,0 +1,52 @@
+	.file	"6-print_line.c"
+	.text
+	.globl	print_line
+	.type	print_line, @function
+print_line:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	$95, %edi
+	call	putchar@PLT
+.L2:
+	movl	-4(%rbp), %eax
+	leal	-1(%rax), %edx
+	movl	%edx, -4(%rbp)
+	testl	%eax, %eax
+	jg	.L3
+	movl	$10, %edi
+	call	putchar@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	print_line, .-print_line
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
