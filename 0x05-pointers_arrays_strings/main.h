4ca079c11d3d1ec6273c51643724421eb7f0e246
AUSTINMORARA <austiinmorara@gmail.com>
2022-09-20 02:03:01 -0700

    pointeers

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#defne MAIN_H
+
+/**
+*void prototypes()
+*int prototypes()
+*/
+
+void reset to_98(int *n);
+void swap int(int *a,int *b);
+int _strlen(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void prints_array(int *a, int n);
+char *_strcoy(char *dest, char *src);
+int _atoi(char *str);
+
+#endif /*MAIN_H*/
+
