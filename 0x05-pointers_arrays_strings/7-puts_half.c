fd4e19875ffc2b74157cb01b89791d0525c72fc3
AUSTINMORARA <austiinmorara@gmail.com>
2022-09-20 05:41:34 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,35 +1,58 @@
-#include <stdio.h>
 #include "main.h"
 
+int _strlen(char *s);
+
 /**
- * puts_half - prints a string
- * @str: string to print
- *
+ * puts_half - function that prints second half of the string
+ *           - if the no. of characters is odd, print n = (length - 1) / 2
+ * @str: string to be manipulated by the function
  * Return: nothing
  */
 
 void puts_half(char *str)
 {
-	int i, len = _strlen(str);
+	int len, len_half_even, len_half_odd;
 
-	for (i = ((len - 1) / 2) + 1; i < len; i++)
-		putchar(*(str + i));
-	putchar(10);
+	len = _strlen(str);
+
+	len_half_even = len / 2;
+	len_half_odd = ((len - 1) / 2) + 1;
+
+	if (len == 0)
+		;
+	else if (len % 2 == 0)
+	{
+		while (len_half_even < len)
+		{
+			_putchar(*(str + len_half_even));
+			len_half_even++;
+		}
+	}
+	else
+	{
+		while ((len_half_odd) < len)
+		{
+			_putchar(*(str + len_half_odd));
+			len_half_odd++;
+		}
+	}
+	_putchar('\n');
 }
 
+
+
 /**
-* _strlen - returns the length of a string
-* @s: string
-*
-* Return: the length of the given string
-*/
+ * _strlen - function that returns the length of a string
+ * @s: string whose length is to be determined by the function
+ * Return: Always 0
+ */
 
 int _strlen(char *s)
 {
-	int len = 0;
+	int a;
 
-	while (*(s + len) != '\0')
-		len++;
+	for (a = 0; *s != '\0'; s++) /*counts characters in string until \0. */
+		a++;
 
-	return (len);
-
+	return (a);
+}
