ee9ea6f5fa49252e68028db6616ce0cf2432862a
AUSTINMORARA <austiinmorara@gmail.com>
2022-09-20 02:11:59 -0700

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
-#defne MAIN_H
+#define MAIN_H
 
 /**
 *void prototypes()
 *int prototypes()
 */
 
-void reset to_98(int *n);
-void swap int(int *a,int *b);
+void reset_to_98(int *n);
+void swap_int(int *a,int *b);
 int _strlen(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
