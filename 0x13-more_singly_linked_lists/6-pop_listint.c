3392e5c7a03a5a3ae3b3a5ad019f79b87ac12ad3
AUSTINMORARA <111016289+AUSTINMORARA@users.noreply.github.com>
2023-01-17 17:17:26 +0300

    Add files via upload

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+*pop_listint - removes first element of list
+*
+*@head: pointer to list
+*
+*Return: data removed
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int output;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+
+	output = temp->n;
+
+	*head = temp->next;
+	free(temp);
+
+	return (output);
+}
