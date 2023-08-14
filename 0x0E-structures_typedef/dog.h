903da88216d46ec4cc508d28d9241d85da379e00
AUSTINMORARA <austiinmorara@gmail.com>
2023-08-14 16:07:17 -0700

    Creation of a structure that represents a dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure represents a dog.
+ * @name: Name of the dog.
+ * @age: age of the dog.
+ * @owner: name of the dogs owner.
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
