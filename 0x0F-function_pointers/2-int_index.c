29353e80cce66595cefee4300134804bbd3fa52d
AUSTINMORARA <austiinmorara@gmail.com>
2023-08-24 07:08:16 -0700

    Function pointer for compare functions returns index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,20 +1,31 @@
 #include "function_pointers.h"
+#include<stdbool.h>
 
 /**
- * int_index - returns index of the array indicated
+ * int_index - searches for a specific integer.
  * @array: array being searched through.
  * @size: size of array to go through.
  * @cmp: pointer to function that compares values.
+ * Return: returns the index of the integer if foundi.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+	bool theone = false;
+
 	if (array)
 	{
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			cmp(array[i]);
+			theone = cmp(array[i]);
+			while (theone)
+			{
+				return (i);
+			}
 		}
-	
+		while (theone == false)
+		{
+			return (-1);
+		}
 	}
 }
