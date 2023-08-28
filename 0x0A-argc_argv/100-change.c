d3e1763c23424bc522ea7e8499465aa9884a6f31
AUSTINMORARA <austiinmorara@gmail.com>
2023-08-28 09:49:31 -0700

    creating  a change machine

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,41 +3,41 @@
 /**
  * main - prints minimum coins to make change.
  * @argc: number of arguments.
- * @argv the arguments representing change and coins
+ * @argv: the arguments representing change and coins
  * Return: Always 0
  */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	int change, count = 0;
 
 	if (argc != 2)
-	{	
+	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
 		change = atoi(argv[1]);
-		while(change > 0)
+		while (change > 0)
 		{
 			if (change >= 25)
 			{
 				change -= 25;
 			}
-			else if(change >= 10)
+			else if (change >= 10)
 			{
 				change -= 10;
 			}
-			else if(change >= 5)
+			else if (change >= 5)
 			{
 				change -= 5;
 			}
-			else if(change >= 2)
+			else if (change >= 2)
 			{
-				change -=2;
+				change -= 2;
 			}
-			else if(change >= 1)
+			else if (change >= 1)
 			{
 				change -= 1;
 			}
