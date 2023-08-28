99e70ef452cd87d68b879bd5972dcb068734a66a
AUSTINMORARA <austiinmorara@gmail.com>
2023-08-28 10:17:56 -0700

    editing file 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,13 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = atoi(argv[1]) * atoi(argv[2]);
+	int res = atoi(argv[1]) * atoi(argv[2]);
 
-	if (argc < 2)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error");
 		return (1);
 	}
-	printf("%d\n", i);
+	printf("%d\n", res);
 	return (0);
 }
