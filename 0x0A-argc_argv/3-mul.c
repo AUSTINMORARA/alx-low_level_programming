da4a7ab357caeacb7a62ee5dc07196751c602815
AUSTINMORARA <austiinmorara@gmail.com>
2023-08-28 07:47:21 -0700

    Editing argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print its argument names.
+ *
+ * @argc: total nummber of arguments.
+ * @argv: array of arguments.
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = atoi(argv[1]) * atoi(argv[2]);
+
+	if (argc < 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%d\n", i);
+	return (0);
+}
