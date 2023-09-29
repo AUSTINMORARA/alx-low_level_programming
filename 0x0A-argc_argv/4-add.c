0fa1e0a1104edc9548a003e6e322dcb775d6f0e7
AUSTINMORARA <austiinmorara@gmail.com>
2023-09-29 14:04:06 -0700

    adding arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,6 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int count, sum;
+	sum = 0;
 
 	if (argc == 1)
 	{
@@ -19,7 +20,6 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		sum = 0;
 		for (count = 1; count < argc; count++)
 		{
 			if (*argv[count] < '0' || *argv[count] > '9')
