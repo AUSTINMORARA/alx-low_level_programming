8bb94f65e3811d8cd83951afd7530df3d292a540
AUSTINMORARA <austiinmorara@gmail.com>
2023-09-29 06:57:15 -0700

    Multiplication of arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: argument counter(number of arguments).
+ * @argv: an array of command line arguments.
+ * Return: 0 on Success.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d", argv[1] * argv[2]);
+	}
+	return (0);
+}
