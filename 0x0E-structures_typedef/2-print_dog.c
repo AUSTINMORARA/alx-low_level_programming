d7a1bc9bf5f751ccf462ca7197a83c11bee9968b
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-09 02:27:30 -0700

    creating a dog structure

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints a struct dog.
+ *
+ * @d: dog to be printed.
+ */
+
+void print_dog(struct dog *d)
+{
+	d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	printf("Name: %s\n", d->name);
+	if (d->age == NULL)
+                printf("Age: (nil)\n");
+	printf("Age: %.2f", d->age);
+	if (d->owner == NULL)
+                printf("Owner: (nil)\n");
+	printf("Owner: %s\n", d->owner);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - Structure representing a dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: dog's owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
