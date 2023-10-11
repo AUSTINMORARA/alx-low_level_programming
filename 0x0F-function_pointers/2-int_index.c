f5d4c14621d4832c290ff06fd3156cc36504ca1b
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-11 07:50:23 -0700

    completed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,21 +6,21 @@
  * @array: array to be searched.
  * @size: size of the array.
  * @cmp: pointer to function that compares values.
+ * Return: index of the number, and -1 if no match
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0;i < size; i++)
 	{
 		if (cmp(array[i]))
-			break;
+			return (i);
 
 	}
-
-	return (i);
+	return (-1);
 }
