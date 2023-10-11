6c717106592ac98ea76c897273f6362f3b632e64
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-11 11:01:21 -0700

    completed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,10 +8,11 @@
  * @argv: commandline arguments.
  * Return: 0 on Success
  */
+
 int main(int argc, char *argv[])
 {
-	char *sign, symbol;
-	int (*operation)(int, int); 
+	char *sign;
+	int (*operation)(int, int);
 	int num1, num2, answer;
 
 	if (argc != 4)
@@ -20,30 +21,19 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	symbol = argv[2][0]
-
-	if ((symbol != '+' ||symbol != '-' ||symbol != '/' ||symbol != '*' ||symbol != '%') || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-                exit(99);
-	}
-
-	num1 = atoi(argv[1]);
-        num2 = atoi(argv[3]);
-
-	if ((symbol == '/' || symbol == '%') && num2 == 0)
-	{
-	 	printf("Error\n");
-                exit(100);
-	}
-
 	sign = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	operation = get_op_func(sign);
 
-        answer = operation(num1, num2);
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	answer = operation(num1, num2);
 
 	printf("%d/n", answer);
-
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,9 +1,12 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * op_add - returns sum of a and b
  * @a: first argument.
  * @b: second argument.
+ * Return: sum
  */
 
 int op_add(int a, int b)
@@ -15,42 +18,56 @@ int op_add(int a, int b)
  * op_sub - returns difference of a and b
  * @a: first argument.
  * @b: second argument.
+ * Return: difference.
  */
 
 int op_sub(int a, int b)
 {
-        return (a - b);
+	return (a - b);
 }
 
 /**
  * op_mul - returns product of a and b
  * @a: first argument.
  * @b: second argument.
+ * Return: product.
  */
 
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 
 /**
  * op_div - returns division result of a and b
  * @a: first argument.
  * @b: second argument.
+ * Return: division result.
  */
 
 int op_div(int a, int b)
 {
-        return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
 /**
  * op_mod - returns the remainder of a and b
  * @a: first argument.
  * @b: second argument.
+ * Return: modulus
  */
 
 int op_mod(int a, int b)
 {
-        return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
