00328ce1a71749d0eb8da4bcf872f13ce83cfeb1
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-11 08:13:55 -0700

    Checking for memory

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,10 +19,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0;i < size; i++)
 	{
 		if (cmp(array[i]) && array[i] != 0)
-			break;
-		if (i == size - 1)
-			return (-1)
+			return (i);
 
 	}
-	return (i);
+	return (-1);
 }
