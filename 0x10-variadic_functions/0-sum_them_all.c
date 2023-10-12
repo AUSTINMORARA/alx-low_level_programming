e179a48839164b5f840df45468b0f791a4b257fc
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-12 10:08:44 -0700

    completed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,15 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int sum, i;
+	int sum;
+	unsigned int i;
 
 	va_start(ap, n);
 
 	sum = 0;
 
+	if (n == 0)
+		return(0);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
