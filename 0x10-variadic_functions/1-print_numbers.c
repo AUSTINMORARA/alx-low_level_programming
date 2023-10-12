45bed7e5a9a38d89cd13b85f55b2bb04f2db3a6e
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-12 10:20:51 -0700

    completion

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,6 +4,7 @@
  * print_numbers - prints numbers
  * @separator: string printed between numbers.
  * @n: number of integers passed.
+ * Return: nothing.
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -16,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		
+
 		if (separator == NULL)
 			return;
 		else if (i != n - 1)
