2c5e842d752938dcd3e6a445acd7d6d64d176605
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-12 10:36:38 -0700

    completed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,10 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		printf("%s", string);
 
-		if (separator == NULL)
-			;
-
-		if (i != n - 1)
+		if (separator != NULL && i != (n - 1))
 		{
 			printf("%s", separator);
 		}
