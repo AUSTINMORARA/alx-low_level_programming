5e66f0a46d86db88f79ecb113bfb047b2ee9f497
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-22 02:37:47 -0700

    Completing function writing

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * square - finds the square root.
+ * @n1: number
+ * @n2: testing number.
+ * Return: square root.
+ */
+
+int square(int n1, int n2)
+{
+	if (n2 * n2 > n1)
+		return(-1);
+	else if (n2 * n2 == n1)
+		return(n2);
+	else
+		return(square(n1, n2 + 1));
+	return(1);
+}
+
+/**
+ * _sqrt_recursion - returns a square root of a number.
+ * @n: number to find the square root for.
+ * Return: square root of the number, -1 if fails.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return(-1);
+	if(n == 1)
+		return(1);
+	return(square(n, 1));
+}
