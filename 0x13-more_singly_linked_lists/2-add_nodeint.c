48b4dce26c344151653073fe52e5842b3c4eec6d
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-24 16:18:25 -0700

    Including NULL  returning in failure

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *temp;
 
 	if (head == NULL)
-		return (0);
+		return (NULL);
 
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
