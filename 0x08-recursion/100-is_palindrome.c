4460f1049f8a587e8718c34b9628f0ef219ec6f0
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-25 12:13:21 -0700

    Palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,8 +6,8 @@ int is_palindrome(char *s);
 
 /**
  * _strlen - returns string length
- * @s: string to measured.
- * Return: The length of the string.
+ * @s: string to find length.
+ * Return: length of the string.
  */
 int _strlen(char *s)
 {
@@ -22,19 +22,16 @@ int _strlen(char *s)
 }
 
 /**
- * check_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
- * @len: The length of s.
+ * check_palindrome - checks if a string is a palindrome.
+ * @s: string to fing length
+ * @len: length of s.
  * @index: The index of the string to be checked.
- *
- * Return: If the string is a palindrome - 1.
- *         If the string is not a palindrome - 0.
+ * Return: if palindrome return 1 else palindrome return 0.
  */
 int check_palindrome(char *s, int len, int index)
 {
 	if (s[index] == s[len / 2])
 		return (1);
-
 	if (s[index] == s[len - index - 1])
 		return (check_palindrome(s, len, index + 1));
 
@@ -42,12 +39,11 @@ int check_palindrome(char *s, int len, int index)
 }
 
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
- *
- * Return: If the string is a palindrome - 1.
- *         If the string is not a palindrome - 0.
+ * is_palindrome - checks if a string is a palindrome.
+ * @s: string to be checked.
+ * Return: if palindrome - 1 else return 0
  */
+
 int is_palindrome(char *s)
 {
 	int i = 0;
