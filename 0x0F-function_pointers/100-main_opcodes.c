2c86d08a514cb2373d7f8b064fadf8c504076ee7
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-25 13:29:28 -0700

    correcting extra errors by (-Werror)

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes
+ * @argc: number of arguments
+ * @argv: array of pointers to the arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int by, i;
+	char *opcode = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	by = atoi(argv[1]);
+
+	if (by < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < by; i++)
+	{
+		printf("%.2x", opcode[i]);
+
+		if (i != by - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
