1be2b3a5aef306b4c8343f4dfe4d03ec91425a14
AUSTINMORARA <austiinmorara@gmail.com>
2023-10-31 16:20:21 -0700

    Creating a copying file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a textfile and prints it to the POSIX.
+ * @filename - name of file being read.
+ * @letters - number of letters read and printed.
+ *
+ * Return: Number of letter read and printed on success else 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file;
+	ssize_t n;
+	char *buffer;
+
+
+	if (filename == NULL)
+		return (0);
+	buffer = malloc((letters + 1) * sizeof(char));
+
+	if (buffer == NULL)
+		return (0);
+
+	buffer[letters] = '\0';
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	n = read(file, buffer, letters);
+
+	write(STDOUT_FILENO, buffer, n);
+	close(file);
+
+	free(buffer);
+	return (n);
+}
+
