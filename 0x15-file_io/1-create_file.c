dd322b357572cd2d42350b6b30d86aef6c6c325e
Austin Morara <111016289+AUSTINMORARA@users.noreply.github.com>
2024-02-22 13:20:46 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,28 +7,32 @@
  *
  * Return: 1 on sucess and -1 on failure.
  */
-
-int create_file(const char *filename, char *text_context)
+int create_file(const char *filename, char *text_content)
 {
-	int i;
-	ssize_t file;
+	int f;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	file = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file == -1)
+	if (f == -1)
 		return (-1);
 
-	if (text_context == NULL)
-		text_context = "";
+	if (!text_content)
+		text_content = "";
 
-	while (text_context[i])
-	{
-		i++;
-	}
-	write(file, text_context, i);
-	close(file);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(f, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(f);
+
 	return (1);
 }
