28a012fedf2be6f9a1cae47bc10e60b9aa0c7f05
AUSTINMORARA <austiinmorara@gmail.com>
2024-06-01 00:19:25 -0700

    counting in a doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in the
+ * doubly linked list
+ *
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
